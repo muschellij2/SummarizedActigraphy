b73923739985b002899fbbf65b4f5d1858790142
muschellij2 <muschellij2@gmail.com>
2021-05-03 11:32:59 -0400

    added the test

--- tests/testthat/test-csv_actigraphy.R
+++ tests/testthat/test-csv_actigraphy.R
@@ -1,8 +1,8 @@
-testthat::test_that("multiplication works", {
+testthat::test_that("using read_actigraphy on CSV files", {
   file = system.file("extdata", "TAS1H30182785_2019-09-17.csv.gz",
                      package = "SummarizedActigraphy")
   out = SummarizedActigraphy::read_actigraphy(file)
-  testthat::expect_true(out$freq == 1L)
+  testthat::expect_true(out$freq == 100L)
   file = system.file("extdata", "example1sec.csv", package = "AGread")
   if (file.exists(file)) {
     out = SummarizedActigraphy::read_actigraphy(file)
